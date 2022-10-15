a1ae8acec67e57927f25394b0da6102e90d3bd37
sheetal L <sheetal@zibtek.in>
2022-10-15 11:34:52 +0530

    code modified

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -20,27 +20,16 @@ class OrderController extends AbstractController
      */
     public function index(): Response
     {
-       // echo 333;
         $response = $this->client->request(
             'GET',
             'https://api.mockaroo.com/api/b51ce860?count=200&key=ba55c120'
-            // 'https://api.mockaroo.com/api/d829bc30?count=1000&key=ba55c120'
         );
-        //var_dump($response);die();
         $statusCode = $response->getStatusCode();
         // $statusCode = 200
-        //echo $statusCode;die();
         $contentType = $response->getHeaders()['content-type'][0];
         // $contentType = 'application/json'
         $content = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
-        //var_dump($content);die();
-        // $content = $response->toArray();
 
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
-       // var_dump($content);die();
-        // return $content;
-      //  return $this->json([$content]);
         $data = json_decode($content, TRUE);
         // var_dump($data[0]['items']);die();
         return $this->render('order/index.html.twig', [
